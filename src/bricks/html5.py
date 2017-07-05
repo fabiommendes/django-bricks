bde85d6a76b596fa27301d5a190b2fccf458cfce
Fabio Teixeira <fabio1079@gmail.com>
2017-07-05 10:44:51 -0300

    Add search for a single element
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Lucas Rufino <lucaslrt@outlook.com>

--- src/bricks/html5.py
+++ src/bricks/html5.py
@@ -23,3 +23,43 @@ def a_or_span(*args, href=None, **kwargs):
         return tags.a(*args, href=href, **kwargs)
     else:
         return tags.span(*args, **kwargs)
+
+
+
+
+def query(current_node, **kwargs):
+    """
+    TODO: Doc this
+    query(node, tag='', classes=[], attrs={'a:': 'b'})
+    from bricks.html5 import div, p, button, query
+    a = div(class_="a") [ p(class_="b")[ button(onclick="alert(123)")[ "click" ] ], div() [ p(class_="c")[ "so texto" ] ] ]
+    """
+
+    node = None
+
+    if 'tag' in kwargs.keys() and current_node.tag_name == kwargs['tag']:
+        node = current_node
+    elif 'attrs' in kwargs.keys():
+        found = 0
+        attrs = kwargs['attrs']
+
+        for key in attrs:
+            if key in current_node.attrs.keys() and \
+            current_node.attrs[key] == attrs[key]:
+                found += 1
+
+        if found == len(kwargs['attrs']):
+            node = current_node
+
+
+    if node is None:
+        for child in current_node.children:
+            if isinstance(child, Text):
+                continue
+
+            found = query(child, **kwargs)
+
+            if found:
+                return found
+
+    return node
