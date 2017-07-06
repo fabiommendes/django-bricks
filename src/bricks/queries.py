ab066632f191c8944b605a8756c3c57cfb35a882
Fabio Teixeira <fabio1079@gmail.com>
2017-07-05 21:43:27 -0300

    Fix bug in query with tag and attrs

--- src/bricks/queries.py
+++ src/bricks/queries.py
@@ -8,7 +8,6 @@ def query_for_attrs(node, attrs):
     If all given attributes were found in the given node,
     then returns the given node otherwise None
 
-
     Ex:
         node = button(class_='btn btn-success', onclick='alert(123)') [ 'Click here !' ]
         query_for_attrs(node, {'onclick': 'alert(123)'})
@@ -65,15 +64,35 @@ def query(current_node, **kwargs):
     """
 
     found = []
+    tag = {
+        'should_verify': 'tag' in kwargs.keys(),
+        'found': False
+    }
+    attrs = {
+        'should_verify': 'attrs' in kwargs.keys(),
+        'found': False
+    }
 
-    if 'tag' in kwargs.keys() and current_node.tag_name == kwargs['tag']:
-        found.append(current_node)
+    if tag['should_verify'] and current_node.tag_name == kwargs['tag']:
+        tag['found'] = True
 
-    if 'attrs' in kwargs.keys():
+    if attrs['should_verify']:
         node = query_for_attrs(current_node, kwargs['attrs'])
 
         if node != None:
-            found.append(node)
+            attrs['found'] = True
+
+    if tag['should_verify'] and attrs['should_verify'] and \
+    tag['found'] and attrs['found']:
+        found.append(current_node)
+
+    elif tag['should_verify'] and not attrs['should_verify'] and \
+    tag['found'] and not attrs['found']:
+        found.append(current_node)
+
+    elif not tag['should_verify'] and attrs['should_verify'] and \
+    not tag['found'] and attrs['found']:
+        found.append(current_node)
 
     found += query_in_children(current_node, **kwargs)
 
